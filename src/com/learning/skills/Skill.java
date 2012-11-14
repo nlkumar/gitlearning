18bb88c16778046e65662a49a7da7d2961e97f5f
Nirmal Madhunagarajan <Nirmal.Madhunagaraja@Gamesys.co.uk>
2012-11-14 14:41:12 +0000

    Revert "skill skillset sop"
    This reverts commit 63c2e4688872653d8aef090b2000c4861a288fa1.

--- src/com/learning/skills/Skill.java
+++ src/com/learning/skills/Skill.java
@@ -6,7 +6,7 @@ public class Skill {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Skill...");
+		// TODO Auto-generated method stub
 
 	}
 	
--- src/com/learning/skills/SkillSet1.java
+++ src/com/learning/skills/SkillSet1.java
@@ -6,7 +6,7 @@ public class SkillSet1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Skill.set ..");
+		// TODO Auto-generated method stub
 
 	}
 	
